32211187643016c4e2a722105f43cf709ba95727
Lucas Eduardo Pereira da Cruz <lucas.cruz54@fatec.sp.gov.br>
2025-09-02 09:23:53 -0300

    Continuação do conteúdo

--- aula04/js/utilidades.js
+++ aula04/js/utilidades.js
@@ -10,6 +10,15 @@ function juntarNome(){
     //1. Concatenação de strings normal
     let nomecompleto = nome + ' ' + sobrenome;
 
-    //2. Injetar o texto da variável nomecompleto na tag <p> no id "nomecompleto"
+    //2. Uso da interpolação strings (template strings)
+    let nomecompleto1 = `<i>${nome} <br>${sobrenome}</i>`;
+
+    //3. Injetar o texto da variável nomecompleto na tag <p> no id "nomecompleto"
     document.getElementById("nomecompleto").innerText = nomecompleto;
+
+    //4. Executar a injeção de tags (elementos HTML) + textos
+    document.getElementById("nomecompleto1").innerHTML = nomecompleto1;
+
+    
 }
+
